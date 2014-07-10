77a98cbf493d4a332513e9a76ecdf1c6002484a8
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-07-10 10:53:23 +0300

    latest autobuilder

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,33 +17,36 @@ class autobuilder_before_install (
 ) {
 
    vcsrepo { "/home/genius/test/yocto-autobuilder":
-	    ensure => present,
-	    provider => git,
-	    source => 'git://git.yoctoproject.org/yocto-autobuilder',
-	    revision => '46485b117ffde2d2255ed3d66d671e2684385035',
-	    user => 'genius'
+            ensure => present,
+            provider => git,
+            source => 'git://git.yoctoproject.org/yocto-autobuilder',
+#           revision => '46485b117ffde2d2255ed3d66d671e2684385035',
+            revision => 'b6f4f74a7475e7180a8e8d1d51b6a4dfb563706d',
+            user => 'genius'
    }
 
    vcsrepo { "/home/genius/test/autobuilder-patches":
-	    ensure => present,
-	    provider => git,
-	    source => 'https://github.com/RobertBerger/autobuilder-patches.git',
-	    user => 'genius'
+            ensure => present,
+            provider => git,
+            source => 'https://github.com/RobertBerger/autobuilder-patches.git',
+            user => 'genius'
    }
 
    include patch
-	
+
    patch::file { '/home/genius/test/yocto-autobuilder/yocto-master/master.cfg':
-	   diff_source => '/home/genius/test/autobuilder-patches/before-install/0001-allow-only-1-build-at-a-time.patch',
+           diff_source => '/home/genius/test/autobuilder-patches/before-install/0001-allow-only-1-build-at-a-time.patch',
+           owner => 'genius',
+           group => 'genius',
    }
-	
-   patch::file { '/home/genius/test/yocto-autobuilder/lib/python2.7/site-packages/autobuilder/buildsteps/MakeImageMD5s.py':
-	  diff_source => '/home/genius/test/autobuilder-patches/before-install/0002-md5sums-for-now.patch',
-   }
+
+ #  patch::file { '/home/genius/test/yocto-autobuilder/lib/python2.7/site-packages/autobuilder/buildsteps/MakeImageMD5s.py':
+#         diff_source => '/home/genius/test/autobuilder-patches/before-install/0002-md5sums-for-now.patch',
+#   }
 
    patch::file { '/home/genius/test/yocto-autobuilder/yocto-autobuilder-setup':
           diff_source => '/home/genius/test/autobuilder-patches/before-install/0003-genius-genius.patch',
+          owner => 'genius',
+          group => 'genius',
    }
 }
-
-
